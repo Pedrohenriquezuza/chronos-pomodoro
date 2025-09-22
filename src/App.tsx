fc4de08031d6d6ae48b6a5ab7056d317bcf34b70
Pedro Zuza <phzuza07@gmail.com>
2025-09-22 18:48:48 -0300

    Aprendendo Sobre useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { Heading } from './components/Heading'
+import "./styles/theme.css";
+import "./styles/global.css";
 
-import './styles/theme.css'
-import './styles/global.css'
+import { Heading } from "./components/Heading";
+import { Container } from "./components/Container/";
+
 export function App() {
-
-    console.log("oi")
-
-    return (
+  return (
     <>
-    <Heading />
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In consequatur dolore officia odit. Fugiat accusantium quae eum doloribus numquam dolorum repudiandae enim harum corporis perspiciatis, dicta optio dolore,</p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+
+      <Container>
+        <section>FORM</section>
+      </Container>
+
+      <Container>
+        <section>FOOTER</section>
+      </Container>
     </>
-)
+  );
 }
